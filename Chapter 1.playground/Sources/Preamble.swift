a7cfdb9409e73ab37101082fa4c5e45e9ce5ef7f
Dion Larson <me@dionlarson.com>
2016-09-15 13:28:19 -0700

    Update to use generic source file

--- Chapter 1.playground/Sources/Preamble.swift
+++
@@ -1,9 +0,0 @@
-// This is the preamble that is shared among all the pages within this chapter.
-// The preamble is at the top-level and can't contain expressions.
-// Wrap everything in a closure!
-
-public var str = "Hello from Make School!"
-
-public func sharedFunc() {
-    print(str)
-}
---
+++ Chapter 1.playground/Sources/SharedSource.swift
@@ -0,0 +1,8 @@
+// This a file shared among all the pages within this chapter.
+// Any files in the Sources folder will be available to all pages.
+
+public var str = "Hello from Make School!"
+
+public func sharedFunc() {
+    print(str)
+}
